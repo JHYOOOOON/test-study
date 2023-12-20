849a095272934dcca372389d0a9fe68a94d5bdf6
JHYOOOOON <jhyooooooon@gmail.com>
2023-12-21 00:53:02 +0900

    chore: mock service worker setup

--- sundaes-on-demand/src/setupTests.js
+++ sundaes-on-demand/src/setupTests.js
@@ -1,5 +1,11 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { server } from "./mocks/server";
+
+// 테스트 전 서버 수신 대기
+beforeAll(() => server.listen());
+
+// 각 테스트가 끝나면 핸들러를 서버 정의 시 핸들러로 재설정
+afterEach(() => server.resetHandlers());
+
+// 전부 끝나면 서버를 닫음
+afterAll(() => server.close());
