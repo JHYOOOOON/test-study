5a8846c05f85a327052df236f02b8fdf77a65d6d
JHYOOOOON <jhyooooooon@gmail.com>
2024-01-02 21:04:41 +0900

    test: OrderEntry 컴포넌트 테스트코드

---
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,24 @@
+import { HttpResponse, http } from "msw";
+import { server } from "../../../mocks/server";
+
+import { render, screen } from "@testing-library/react";
+import OrderEntry from "../OrderEntry";
+
+test("handle error for scoops and toppings routes", async () => {
+  server.resetHandlers(
+    http.get("http://localhost:3030/scoops", () => {
+      return new HttpResponse(null, { status: 500 });
+    }),
+    http.get("http://localhost:3030/toppings", () => {
+      return new HttpResponse(null, { status: 500 });
+    })
+  );
+
+  render(<OrderEntry />);
+
+  const alerts = await screen.findAllByRole("alert", {
+    name: "An unexpected error occured. Please try again later.",
+  });
+
+  expect(alerts).toHaveLength(2);
+});
