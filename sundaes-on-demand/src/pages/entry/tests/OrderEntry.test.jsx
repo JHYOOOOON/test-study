7555c1f7ecd340280397a3aa9624a87d57b3a9ef
JHYOOOOON <jhyooooooon@gmail.com>
2024-01-02 21:28:06 +0900

    fix: 테스트코드 수정

--- sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -4,7 +4,7 @@ import { server } from "../../../mocks/server";
 import { render, screen } from "@testing-library/react";
 import OrderEntry from "../OrderEntry";
 
-test("handle error for scoops and toppings routes", async () => {
+test("handles error for scoops and toppings routes", async () => {
   server.resetHandlers(
     http.get("http://localhost:3030/scoops", () => {
       return new HttpResponse(null, { status: 500 });
@@ -16,9 +16,6 @@ test("handle error for scoops and toppings routes", async () => {
 
   render(<OrderEntry />);
 
-  const alerts = await screen.findAllByRole("alert", {
-    name: "An unexpected error occured. Please try again later.",
-  });
-
+  const alerts = await screen.findAllByRole("alert");
   expect(alerts).toHaveLength(2);
 });
